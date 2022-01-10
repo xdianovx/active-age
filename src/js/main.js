dd4fee6f8bc40437c2fb6c28da980ec0513ce503
Евгений Дианов <dianov.js@gmail.com>
2022-01-10 23:15:46 +0300

    Nutrition десктоп

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,29 @@
-import 'focus-visible';
-import lazyImages from './modules/lazyImages';
-import documentReady from './helpers/documentReady';
+document.addEventListener('DOMContentLoaded', () => {
+  const tabs = document.querySelector('.tabs')
+  const tabsBtn = document.querySelectorAll('.tabs__btn')
+  const tabsContent = document.querySelectorAll('.tabs__content')
 
-documentReady(() => {
-  lazyImages();
-});
+  if (tabs) {
+    tabs.addEventListener('click', (e) => {
+      if (e.target.classList.contains('tabs__btn')) {
+        const {tabsPath} = e.target.dataset
+        tabsBtn.forEach((el) => {
+          el.classList.remove('active')
+        })
+        document
+            .querySelector(`[data-tabs-path="${tabsPath}"]`)
+            .classList.add('active')
+        tabsHandler(tabsPath)
+      }
+    })
+  }
+
+  const tabsHandler = (path) => {
+    tabsContent.forEach((el) => {
+      el.classList.remove('active')
+    })
+    document
+        .querySelector(`[data-tabs-target="${path}"]`)
+        .classList.add('active')
+  }
+})
